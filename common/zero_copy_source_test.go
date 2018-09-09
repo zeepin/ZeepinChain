d5c03824b53e4612b11f23cbb9de5aa740585479
leoliu1027 <149067623@qq.com>
2018-09-09 15:47:07 +0800

    update name

--- common/zero_copy_source_test.go
+++ common/zero_copy_source_test.go
@@ -13,7 +13,7 @@
  * GNU Lesser General Public License for more details.
  *
  * You should have received a copy of the GNU Lesser General Public License
- * along with The ontology.  If not, see <http://www.gnu.org/licenses/>.
+ * along with The zeepinchain.  If not, see <http://www.gnu.org/licenses/>.
  */
 package common
 
